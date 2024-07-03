e0200db2eb2ff35c4b2e9992476d414126e379f7
dcronk13 <davidcronk99@gmail.com>
2024-07-02 20:15:14 -0700

    Update searchEvosByName.js

---
+++ public/searchEvosByName.js
@@ -0,0 +1,11 @@
+function searchPokeName() {
+    //used by evolutions page to get value of name search string. sets the url so JS script can read and perform functions
+    var poke_name_search_string  = document.getElementById('poke_name_search_string').value
+    if (poke_name_search_string != ''){
+        window.location = '/evolutions/search/' + encodeURI(poke_name_search_string)
+    }
+    //if value found is empty, return user to main page with all values (used to undo search)
+    else {
+        window.location = '/evolutions'
+    }
+}
