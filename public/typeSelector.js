b8ebbca0fc1adb291589e0270d04176dad5499bf
dcronk13 <davidcronk99@gmail.com>
2024-07-02 20:17:16 -0700

    Update typeSelector.js

---
+++ public/typeSelector.js
@@ -0,0 +1,5 @@
+function selectTypes(id1,id2){
+    //used in update pokes page to set drop-down default select to type pokemon already is
+    $("#type_selector_1").val(id1);
+    $("#type_selector_2").val(id2);
+}
