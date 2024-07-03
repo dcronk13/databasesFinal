66b495f4318f525e533deef4c148333d5fda0711
dcronk13 <davidcronk99@gmail.com>
2024-07-02 20:16:58 -0700

    Update selectEvo.js

---
+++ public/selectEvo.js
@@ -0,0 +1,6 @@
+function selectPokes(id1,id2,id3){
+    //used in update evos page to set drop-down default select to pokemon the evos are already
+    $("#poke").val(id1);
+    $("#poke_pre").val(id2);
+    $("#poke_post").val(id3);
+}
