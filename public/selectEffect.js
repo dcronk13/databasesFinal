82b52e179c5ac02259a1424f3d54638f2372d44b
dcronk13 <davidcronk99@gmail.com>
2024-07-02 20:16:33 -0700

    Update selectEffect.js

---
+++ public/selectEffect.js
@@ -0,0 +1,21 @@
+function selectEffects(id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id13,id14,id15,id16,id17,id18){
+    //Used in update types to set select values to current values of that type before updating
+    $("#Normal").val(id1);
+    $("#Fire").val(id2);
+    $("#Water").val(id3);
+    $("#Grass").val(id4);
+    $("#Electric").val(id5);
+    $("#Ice").val(id6);
+    $("#Fighting").val(id7);
+    $("#Poison").val(id8);
+    $("#Ground").val(id9);
+    $("#Flying").val(id10);
+    $("#Psychic").val(id11);
+    $("#Bug").val(id12);
+    $("#Rock").val(id13);
+    $("#Ghost").val(id14);
+    $("#Dark").val(id15);
+    $("#Dragon").val(id16);
+    $("#Steel").val(id17);
+    $("#Fairy").val(id18);
+}
